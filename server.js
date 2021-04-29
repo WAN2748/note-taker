1c74d681ee3110f92cb8d9fa3f62c7f8b11946eb
Winston <niera.w.a@gmail.com>
2021-04-29 18:31:24 -0400

    fix file paths

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const express = require('express');
 const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -20,8 +20,8 @@ app.use(express.json());
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 app.use(express.static('public'));
-require('../routes/apiRoutes')(app);
-require('../routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 // LISTENER
 // The below code effectively "starts" our server
